d866185262ab7ad82fedd14d7ab564763e05b8b1
Chimello44 <hugoluizchimello@gmail.com>
2015-03-18 11:08:08 -0300

    imagens

--- Navigation/MySingleton.m
+++ Navigation/MySingleton.m
@@ -31,7 +31,7 @@ static MySingleton *single=nil;
         letter=[[NSArray alloc]initWithObjects:@"A",@"B",@"C",@"D",@"E",@"F",@"G",@"H",@"I",@"J",@"K",@"L",@"M",@"N",@"O",@"P",@"Q",@"R",@"S",@"T",@"U",@"V",@"W",@"X",@"Y",@"Z", nil];
         
         word = [[NSArray alloc] initWithObjects:@"Astra",@"Bentley",@"Cadillac",@"DeLorean",@"Eclipse",@"Ferrari",@"Gurgel",@"Hummer",@"Impala",@"Jaguar",@"Kombi",@"Lincoln",@"McLaren",@"Neon",@"Opala",@"Parati",@"Qashqai",@"Ranger",@"Santana",@"Thunderbird",@"Uno",@"Veraneio",@"Wagoneer",@"Xsara",@"Yaris",@"Zagatto", nil];
-        img = [[NSArray alloc] initWithObjects:@"Apple.jpg",@"Balloon.jpg",@"Cadillac",@"DeLorean",@"Eclipse",@"Ferrari",@"Gurgel",@"Hummer",@"Impala",@"Jaguar",@"Kombi",@"Lincoln",@"McLaren",@"Neon",@"Opala",@"Parati",@"Qashqai",@"Ranger",@"Santana",@"Thunderbird",@"Uno",@"Veraneio",@"Wagoneer",@"Xsara",@"Yaris",@"Zagatto", nil];
+        img = [[NSArray alloc] initWithObjects:@"Astra.jpg",@"Bentley.jpg",@"Cadillac.jpg",@"DeLorean.jpg",@"Eclipse.jpg",@"Ferrari.jpg",@"Gurgel.jpg",@"Hummer.jpg",@"Impala.jpg",@"Jaguar.jpg",@"Kombi.jpg",@"Lincoln.jpg",@"McLaren.jpg",@"Neon.jpg",@"Opala.jpg",@"Parati.jpg",@"Qashqai.jpg",@"Ranger.jpg",@"Santana.jpg",@"Thunderbird.jpg",@"Uno.jpg",@"Veraneio.jpg",@"Wagonner.jpg",@"Xsara.jpg",@"Yaris.jpg",@"Zagatto.jpg", nil];
         
         
         contador=0;
